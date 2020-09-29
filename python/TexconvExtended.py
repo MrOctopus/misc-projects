f07c63cd8f9bfb91a00d9a9bba3aacb583bc3e19
MrOctopus <shock.game@hotmail.com>
2020-09-29 09:10:27 +0200

    Hopefully fixed bugs that were added in newer versions of windows

--- python/TexconvExtended.py
+++ python/TexconvExtended.py
@@ -1,59 +1,69 @@
 #!/usr/bin/env python
 
 __author__ = "NeverLost"
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
-import os, sys, subprocess
+import os
+import sys
+import subprocess
 
-# CHANGE THIS TO THE PATH OF TEXCONV.EXE
-PATH_TEXCONV = r"C:\Users\MAX\Desktop\Misc\Texconv Test\Texconv.exe"
+# CHANGE THIS TO THE PATH OF TEXCONV
+PATH_TEXCONV = r"C:\Users\MrOctopus\Desktop\Misc\Texconv.exe"
 
 # IGNORE EVERYTHING BELOW
 VALID_FORMATS = [".BMP", ".JPG", ".JPEG", ".PNG", ".DDS", ".TGA", ".HDR", ".TIF", ".TIFF", ".WDP", ".HDP", ".JXR"]
 
-overwrite_output = False
+# Quit with error message
+def error_quit(error : str, err_code : int):
+    print("[ERROR] {}".format(error))
+    quit(err_code)
 
-def traverse(dir_path, name_file, args):
-    for objname in os.listdir(dir_path):
-        obj = os.path.join(dir_path, objname)
-
-        if os.path.isdir(obj):
-            traverse(obj, name_file, args)
+# Run the extended version of texconv
+def extended_run(args, maintain_folders : bool):
+    path = os.path.abspath(args[-1])
+    ext = os.path.splitext(path)[1].upper()
     
-    if overwrite_output:
-        args[1] = dir_path
-        
-    args[-1] = os.path.join(dir_path + os.sep, name_file)
-    subprocess.call([PATH_TEXCONV] + args)
+    if not ext in VALID_FORMATS:
+        error_quit("Failed to read {}".format(args[-1]), 2)
+
+    path = path.rpartition(os.sep)
+
+    for root, dirs, files in os.walk(path[0]):
+        if maintain_folders:
+            args[1] = root
+            
+        args[-1] = os.path.join(root, path[2])
+        subprocess.run([PATH_TEXCONV] + args)
 
 def main():
     print("")
     print("TEXCONV EXTENDED")
     print("----------------")
 
-    tmp_args = [each_string.lower() for each_string in sys.argv]
+    if not os.path.isfile(PATH_TEXCONV):
+        error_quit("Could not find texconv", 1)
 
-    if len(sys.argv) == 1:
-        subprocess.call([PATH_TEXCONV])
+    # Remove filename from args, create tmp_args
+    # and check for flags
+    args = sys.argv[1:]
+    tmp_args = [each_string.lower() for each_string in args]
+
+    if len(args) == 0:
+        subprocess.run([PATH_TEXCONV])
         
     elif tmp_args.count('-r') == 0:
-        subprocess.call([PATH_TEXCONV] + sys.argv[1:])
+        subprocess.run([PATH_TEXCONV] + args)
         
     else:
-        del sys.argv[tmp_args.index('-r')]
+        del args[tmp_args.index('-r')]
         
-        if tmp_args.count('-o') == 0:
-            overwrite_output = True
-            sys.argv.insert(1, '-o')
-            sys.argv.insert(2, 'tmp')
+        maintain_folders = False
 
-        path_abs = os.path.abspath(sys.argv[-1])
+        if tmp_args.count('-o') == 0:
+            args.insert(0, '-o')
+            args.insert(1, 'tmp')
+            maintain_folders = True
         
-        if os.path.splitext(path_abs)[1].upper() in VALID_FORMATS:
-            path_delim = path_abs.rpartition(os.sep)
-            traverse(path_delim[0], path_delim[2], sys.argv[1:])
-            
-        else:
-            print("reading "+sys.argv[-1]+" FAILED")
+        extended_run(args, maintain_folders)
             
 main()
