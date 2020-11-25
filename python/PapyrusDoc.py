6bf57031eec3cafba0628dad1c14d3aad0c58309
MrOctopus <shock.game@hotmail.com>
2020-11-25 09:28:53 +0100

    Refactored while loops to use Walrus operator Py 3.8+, next up is implementing doc reader

--- python/PapyrusDoc.py
+++ python/PapyrusDoc.py
@@ -5,16 +5,16 @@ __version__ = "1.0.0"
 
 import sys
 import os.path
+import enum
 
-# len("Scriptname ") = 11
-EXPECTED_NAME_INDEX = 11
-
-class Type:
-    PROPERTY = "property"
-    EVENT = "event"
-    FUNCTION = "function"
+class Type(enum):
+    SCRIPT = 0
+    PROPERTY = 1
+    EVENT = 2
+    FUNCTION = 3
 
 class Doc:
+    TYPES_DOC("scriptname", "property", "event", "function")
     START_DOC = '{'
     END_DOC = '}'
 
@@ -22,31 +22,27 @@ class Doc:
     def from_file(file):
         previous_line = ""
 
-        while True:
-            line = file.readline()
-
-            if not line:
-                throw Exception
-
+        while line := file.readline():
             line = line.strip()
 
             if line[0] is Doc.START_DOC:
+                # Read different types here
+                # Throw exceptions on malformed comments
                 pass
             else:
                 previous_line = line
 
-    def __init__(self, name, name_full, description = "", params = None, return_val = ""):
+        # EOF
+        return None
+
+    @from_line
+
+    def __init__(self, name, name_full, description = None, params = None, return_val = None):
         self.name = name
         self.name_full = name_full
-        
-        if description:
-            self.description = description
-
-        if params:
-            self.params = params
-
-        if return_val:
-            self.return_val = return_val
+        self.description = description
+        self.params = params
+        self.return_val = return_val
         
     def __eq__(self, other):
         return self.name == other.name
@@ -59,20 +55,37 @@ class Doc:
         return ""
 
 class File_Doc:
+    # len("Scriptname ") = 11
+    EXPECTED_NAME_INDEX = 11
+
     @classmethod
     def from_file(file_name : str, file):
         line = file.readline()
         
         if not line:
-            throw Exception
+            throw Exception()
             
-        if line.lower().find(file_name.lower()) != 11:
-            throw Exception
-            
-        #return cls(Doc_File(file_name, file, line))
+        if line.lower().find(file_name.lower()) != cls.EXPECTED_NAME_INDEX:
+            throw Exception()
+        
+        file.seek(0, 0)
+        doc = Doc.from_file(file)
+
+        if not doc
+            file_doc = cls(Doc(file_name, line))
+        elif not doc.type is Type.SCRIPT:
+            file_doc = cls(Doc(file_name, line))
+            file_doc.add(doc)
+        else:
+            file_doc = cls(doc)
+
+        return file_doc
 
     def __init__(self, doc):
         # Primary
+        if not isinstance(doc, Doc):
+            throw Exception()
+
         self.doc = doc
         
         # Secondary
@@ -85,7 +98,7 @@ class File_Doc:
     
     def add(self, doc):
         if not isinstance(doc, Doc):
-            return
+            throw Exception()
         
         if doc.type is Type.PROPERTY
             self.properties.append(doc)
@@ -112,25 +125,18 @@ def generate_md(doc_data):
 
 def parse_file(file_path : str):
     if not file_path.lower().endswith(".psc"):
-        return
+        throw Exception()
     
     if not path.isfile(file_path):
-        return
+        throw Exception()
     
     file_name = path.splitext(path.basename(file_path))[0]
     
     with open(file_path, 'r') as file:
-        try:
-            file_doc = File_Doc(file_name, file)
-        except Exception:
-            return
+        file_doc = File_Doc.from_file(file_name, file)
 
-        while True:
-            try:
-                doc = Doc.from_file(file)
-                file_doc.add(doc)
-            except Exception:
-                break
+        while doc := Doc.from_file(file):            
+            file_doc.add(doc)
 
 def main(file_path : str):  
     if len(sys.argv) == 1:
