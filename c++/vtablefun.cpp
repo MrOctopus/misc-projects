ab8d763358639a1b74589cbc90761f4ce509b953
MrOctopus <shock.game@hotmail.com>
2021-01-29 21:47:21 +0100

    Nl_marriage and c++ vtable fun

---
+++ c++/vtablefun.cpp
@@ -0,0 +1,29 @@
+/*-------------------------------------------------------
+
+@author NeverLost
+
+Virtual table fun. 
+Let's call Test::hey() directly from the vtable.
+
+---------------------------------------------------------*/
+
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+class Test 
+{
+public:
+    int ignore;
+    virtual void hey() { cout << "\nHey\n"; }
+};
+
+int main()
+{
+    Test obj;
+    uintptr_t* pointer = (uintptr_t*) &obj;
+    pointer = (uintptr_t*) *pointer;
+    ((void (*)()) *pointer)();
+    return 0;
+}
